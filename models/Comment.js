de546e47c72677d2b6e9c0dfe48df3338dbc411b
Deepika Bekal <deepika.bekal2@gmail.com>
2021-06-26 23:48:07 +0400

    comment reply schema created

--- models/Comment.js
+++ models/Comment.js
@@ -1,19 +1,64 @@
-const { Schema, model} = require ('mongoose');
+const { Schema, model, Types} = require ('mongoose');
+const dateFormat = require ('../utils/dateFormat');
 
-const CommentSchema = new Schema ({
-    writtenBy : {
-        type : String
+const ReplySchema = new Schema (
+    {
+        //set custom replyId to avoid confusion with the parent comment id
+        replyId : {
+            type : Schema.Types.ObjectId(),
+            default : () => new Types.ObjectId()
+        },
+
+        replyBody : {
+            type : String, 
+        },
+
+        writtenBy : {
+            type : String,
+        },
+
+        createdAt : {
+            type : Date,
+            default : Date.now,
+            get : (createdAtVal) => dateFormat(createdAtVal)
+        }
     },
-
-    commentBody : {
-        type : String
-    }, 
-
-    createdAt : {
-        type : Date,
-        default : Date.now
+    {
+        toJSON : {
+            getters : true
+        }
     }
+);
 
+const CommentSchema = new Schema (
+    {
+        writtenBy : {
+            type : String
+        },
+
+        commentBody : {
+            type : String
+        }, 
+
+        createdAt : {
+            type : Date,
+            default : Date.now,
+            get : (createdAtVal) => dateFormat(createdAtVal)
+        },
+
+        // use ReplySchema to validate data for a reply
+        replies : [ReplySchema]
+    },
+    {
+        toJSON : {
+            getters : true,
+            virtuals : true
+        }
+    }
+);
+
+CommentSchema.virtuals('replyCount').get(function () {
+    return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
--- models/Pizza.js
+++ models/Pizza.js
@@ -38,7 +38,7 @@ const PizzaSchema = new Schema (   //Schema --> constructor imported from mongoo
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function() {
-    return this.comments.length;
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
 // create the Pizza model (whihc means table) using the PizzaSchema
